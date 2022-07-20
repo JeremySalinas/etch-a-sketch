a2aec9b567a0d82d49086f6d98cf7bb6cfb9518e
[Jeremy] <[salinasjeremy2@gmail.com]>
2022-07-19 23:19:11 -0500

    finished game

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 function etchsketch(numOfSquares){
+
   for(let j=0;j<numOfSquares;j++){
     let currentrow = document.createElement('p')
-    document.getElementById('pixels').appendChild(currentrow)
+    document.getElementById('pixels').append(currentrow)
 
     for(let y=0;y<numOfSquares;y++){
       let div = document.createElement('div')
-      currentrow.appendChild(div)
+      currentrow.append(div)
       div.classList.add('unhover')
     }
   }
 
+  let color = "black"
+  let oldColor
+
+  document.getElementById('colors').addEventListener('click', e =>{
+    oldColor = color
+    if(e.target.innerText === 'red')
+      color = "red"
+    if(e.target.innerText === 'green')
+      color = "green"
+    if(e.target.innerText === 'blue')
+      color = "blue"
+    if(e.target.innerText === 'black')
+      color = 'black'
+    if(e.target.innerText ==='ereaser')
+      color = 'white'
+  })
 
   for(let i=0;i<(numOfSquares * numOfSquares);i++){
     document.getElementsByClassName('unhover')[i].addEventListener('mouseover', e =>{
-      document.getElementsByClassName('unhover')[i].classList.add("permahover");
+      document.getElementsByClassName('unhover')[i].classList.remove(oldColor);
     })
-}
+  }
+
+  for(let i=0;i<(numOfSquares * numOfSquares);i++){
+    document.getElementsByClassName('unhover')[i].addEventListener('mouseover', e =>{
+      document.getElementsByClassName('unhover')[i].classList.add(color);
+    })
+  }
 
 }
 
