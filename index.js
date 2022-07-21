a052474d5579c80de25ec29934c49fff928277ef
Jeremy <salinasjeremy2@gmail.com>
2022-07-20 18:19:36 -0700

    major UI changes

--- index.js
+++ index.js
@@ -11,32 +11,60 @@ function etchsketch(numOfSquares){
     }
   }
 
-  let color = "black"
-  let oldColor
+  let color
+  let red=0, green=0, blue=0
+  let rgbBOX = document.querySelector('.rgbBOX')
+  let randomColor
+  let random = false
+  let j = 0
+
+  rgbBOX.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
 
   document.getElementById('colors').addEventListener('click', e =>{
-    oldColor = color
-    if(e.target.innerText === 'red')
-      color = "red"
-    if(e.target.innerText === 'green')
-      color = "green"
-    if(e.target.innerText === 'blue')
-      color = "blue"
-    if(e.target.innerText === 'black')
-      color = 'black'
-    if(e.target.innerText ==='ereaser')
-      color = 'white'
+    console.log(e.target.className)
+    console.log(random)
+    
+    if(e.target.id === 'red')
+      red = e.target.value
+    if(e.target.id === 'green')
+    green = e.target.value
+    if(e.target.id === 'blue')
+    blue = e.target.value
+
+    if(e.target.className === 'black'){
+      red = 0
+      green = 0
+      blue = 0
+    }
+    if(e.target.className ==='eraser'){
+      red = 255
+      green = 255
+      blue = 255
+    }
+
+    if(e.target.className === 'random'){
+      random = true
+    }else{
+      random = false
+    }
+
+    rgbBOX.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+
   })
-
+  
   for(let i=0;i<(numOfSquares * numOfSquares);i++){
     document.getElementsByClassName('unhover')[i].addEventListener('mouseover', e =>{
-      document.getElementsByClassName('unhover')[i].classList.remove(oldColor);
-    })
-  }
-
-  for(let i=0;i<(numOfSquares * numOfSquares);i++){
-    document.getElementsByClassName('unhover')[i].addEventListener('mouseover', e =>{
-      document.getElementsByClassName('unhover')[i].classList.add(color);
+      color = document.getElementsByClassName('unhover')[i]
+      if(random){
+        j++
+        randomColor = `rgb(${(Math.random() * 255) +1}, ${(Math.random() * 255) +1}, ${(Math.random() * 255) +1})`
+        if(j%10 === 0)
+          randomColor = 'black'
+        color.style.backgroundColor = randomColor
+        rgbBOX.style.backgroundColor = randomColor
+      }else{
+        color.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+      }
     })
   }
 
